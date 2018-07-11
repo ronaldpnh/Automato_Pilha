80cf03ebe6649ec63a53d0694908e3d5d6ebd62c
Ronald Pinho <ronaldppinho@gmail.com>
2018-07-11 12:50:18 -0300

    Concluido

--- main.py
+++ main.py
@@ -33,57 +33,86 @@ def leRegrasDeTransicao(automato):
 		regras.append(automato[i])
 	return regras
 
+'''
 def testepv(estado, regras, pilha):
 	# estado atual, conjunto de regras e a pilha
 	for regra in regras:
-		if (estado == regra[0]) and (regra[1] == "?") and (regra[2] == "?"):
+		if (estado == regra[0]) and (regra[1] == "?") and (r_pilha == "?"):
 			if pilha == []:
 				print("(" + regra[0] + ", " + regra[1] +", "+ 
-					regra[2] + ") = ("+ regra[3] +", "+ regra[4]+
+					r_pilha + ") = ("+ prox_estado +", "+ w_pilha+
 					") - Teste da pilha vazia [YES]")
 				return True
 			else:
 				print("(" + regra[0] + ", " + regra[1] +", "+ 
-					regra[2] + ") = ("+ regra[3] +", "+ regra[4]+
+					r_pilha + ") = ("+ prox_estado +", "+ w_pilha+
 					") - Teste da pilha vazia [NOT]")
 				return False
+'''
+
+
+def printar(estado, letra, r_pilha, prox_estado, w_pilha, pilha):
+	if w_pilha != "_":
+		if len(w_pilha)>1:
+			for c in w_pilha:
+				pilha.append(c)
+		else:
+			pilha.append(w_pilha)
+			
+	print("(" + estado + ", " + letra +", "+ r_pilha + ") = ("+ prox_estado +", "+ w_pilha+ ") - Pilha = {}".format(pilha))
+	return (True,prox_estado)
+
+
+def testepv(pilha, r_pilha, letra):
+	if (len(pilha)==0) and (r_pilha=="?") and (letra == " "):
+		return True
+	return False
 
 def leituraDaPalavra(inicial, finais, regras, alfapilha):
 	palavra = sys.argv[2]
+	palavra += " "
 	estado_atual = inicial[0]
 	pilha = []
 
-	i = 0
+	
 	for letra in palavra:
 		validacao = False
 
 		for regra in regras:
-			if (regra[0] == estado_atual) and (regra[1] == letra):
+			estado, simbolo, r_pilha, prox_estado, w_pilha = regra
+			if (estado==estado_atual) and ((simbolo == letra) or (simbolo=="?")):				
+				#print(regra)
+				if testepv(pilha, r_pilha, letra):
+					validacao, estado_atual = printar(estado, letra, r_pilha, prox_estado, w_pilha, pilha)
+					break
 
-				if regra[4] != "_":
-					pilha.append(regra[4])
 
-				estado_atual = regra[3]
+				elif r_pilha == "_":
+					validacao, estado_atual = printar(estado, letra, r_pilha, prox_estado, w_pilha, pilha)
+					break
 
-				if regra[2] != "_":
-					if (pilha != []) and (regra[2] == pilha[len(pilha)-1]):
+				elif (len(pilha)>0) and (r_pilha==pilha[-1]):
+					pilha.pop()
+					validacao, estado_atual = printar(estado, letra, r_pilha, prox_estado, w_pilha, pilha)
+					break
+
+				
+				"""
+				if r_pilha != "_":
+					if (pilha != []) and (r_pilha == pilha[-1]):
 						lido = pilha.pop()
 					else:
 						print("Não é possível desempilhar: Pilha Vazia ou Elemento não existente")
 						return False
-
-				print("(" + estado_atual + ", " + letra +", "+ 
-					regra[2] + ") = ("+ regra[3] +", "+ regra[4]+
-					") - Pilha = {}".format(pilha))
-
-				validacao = True
-				break
-		i += 1
+				"""
 
 		if not validacao:
 			return False
 
-	return testepv(estado_atual, regras, pilha)
+	if estado_atual in finais:
+		return True
+	else:
+		return False
 
 
 def main():
---
+++ teste.py
@@ -0,0 +1,9 @@
+lista = []
+validacao=False
+def inicia(lista, x):
+	lista.append(x)
+	validacao = True
+
+inicia(lista, "g")
+print(lista,validacao)
+
